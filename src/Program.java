8da696688187952e7c073f7468be05bad5342826
Ramon Chiara <ramon@ramon.pro.br>
2017-03-23 20:33:48 -0300

    Solução do Bhaskara às Avessas usando nextLine

--- src/Program.java
+++ src/Program.java
@@ -1,26 +1,24 @@
-
 import java.util.Scanner;
 
 public class Program {
 
+    // PROGRAMA DO BHASKARA AS AVESSAS
+    
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
-        // ENTRADA
         System.out.print("x1: ");
-        double x1 = console.nextDouble();
+        double x1 = Double.parseDouble(console.nextLine());
         System.out.print("x2: ");
-        double x2 = console.nextDouble();
+        double x2 = Double.parseDouble(console.nextLine());
         System.out.print("c: ");
-        double c = console.nextDouble();
+        double c = Double.parseDouble(console.nextLine());
 
-        // PROCESSAMENTO
         double s = x1 + x2;
         double p = x1 * x2;
         double a = c / p;
         double b = -(s * a);
 
-        // SAÍDA
         System.out.println("A equação que tem raizes " + x1 + " e " + x2 + " é " + a + "x² + " + b + "x + " + c);
     }
 }
